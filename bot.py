a302f274988be97723383bf910377ed0ef6da848
svillors <tntzator9@gmail.com>
2025-08-03 23:45:50 +0500

    Add ConverstionHandler

--- bot.py
+++ bot.py
@@ -2,11 +2,13 @@ import logging
 import os
 import re
 import random
+from enum import Enum
 
 import redis
 from dotenv import load_dotenv
 from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
+                          CallbackContext, ConversationHandler)
 
 
 logging.basicConfig(
@@ -14,6 +16,11 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
+
+
+class State(Enum):
+    NEW_QUESTION = 1
+    SOLUTION_ATTEMPT = 2
 
 
 def get_questions(path):
@@ -40,40 +47,38 @@ def get_questions(path):
 def start(update: Update, context: CallbackContext) -> None:
     keyboard = ReplyKeyboardMarkup([
         [KeyboardButton('Новый вопрос')],
-        [KeyboardButton('Сдаться')],
-        [KeyboardButton('Мой счёт')]
+        [KeyboardButton('Сдаться')]
     ])
-    user = update.effective_user
-    update.message.reply_markdown_v2(
-        fr'Hi {user.mention_markdown_v2()}\!',
+    update.message.reply_text(
+        'Привет! Я бот-викторина. Нажми "Новый вопрос", чтобы начать.',
         reply_markup=keyboard,
     )
 
-
-def help_command(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Help!')
+    return State.NEW_QUESTION.value
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    message = update.message.text
+def handle_new_question_request(update, context):
     questions = context.bot_data.get("questions", {})
     db = context.bot_data.get('db')
-    if message == 'Новый вопрос':
-        question_number = random.randint(1, len(questions))
-        question = questions[f'question{question_number}']
-        db.set(update.effective_user.id, question['answer'])
-        update.message.reply_text(question['question'])
-    elif message == 'Сдаться':
-        pass
-    elif message == 'Мой счёт':
-        pass
+    question_number = random.randint(1, len(questions))
+    question = questions[f'question{question_number}']
+    db.set(update.effective_user.id, question['answer'])
+    update.message.reply_text(question['question'])
+
+    return State.SOLUTION_ATTEMPT.value
+
+
+def handle_solution_attempt(update, context):
+    db = context.bot_data.get('db')
+    answer = db.get(update.effective_user.id).decode('utf-8')
+    message = update.message.text
+    if message.lower() == re.sub(r'\s*\([^)]*\)|[.]+$', '', answer).lower():
+        update.message.reply_text('Правильно! Поздравляю! Для ' \
+                                  'следующего вопроса нажми «Новый вопрос')
+        return State.NEW_QUESTION.value
     else:
-        answer = db.get(update.effective_user.id).decode('utf-8')
-        if message.lower() == re.sub(r'\s*\([^)]*\)|[.]+$', '', answer).lower():
-            update.message.reply_text('Правильно! Поздравляю! Для ' \
-                                      'следующего вопроса нажми «Новый вопрос')
-        else:
-            update.message.reply_text('Неправильно… Попробуешь ещё раз?')
+        update.message.reply_text('Неправильно… Попробуешь ещё раз?')
+        return State.SOLUTION_ATTEMPT.value
 
 
 def main() -> None:
@@ -90,10 +95,21 @@ def main() -> None:
     dispatcher.bot_data['questions'] = questions
     dispatcher.bot_data['db'] = db
 
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            State.NEW_QUESTION.value: [
+                MessageHandler(Filters.regex('Новый вопрос'), handle_new_question_request),
+            ],
+            State.SOLUTION_ATTEMPT.value: [
+                MessageHandler(Filters.regex('Новый вопрос'), handle_new_question_request),
+                MessageHandler(Filters.text & ~Filters.command, handle_solution_attempt)
+            ],
+        },
+        fallbacks=[CommandHandler("start", start)],
+    )
 
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(conv_handler)
 
     updater.start_polling()
     updater.idle()
