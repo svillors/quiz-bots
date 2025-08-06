6226dd2c8705723638a64970a7bd83ab729bd6ec
svillors <tntzator9@gmail.com>
2025-08-06 17:42:22 +0500

    Create similar bot for vk

---
+++ vk_bot.py
@@ -0,0 +1,93 @@
+import os
+import re
+import random
+
+import redis
+import vk_api as vk
+from vk_api.keyboard import VkKeyboard
+from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.utils import get_random_id
+from dotenv import load_dotenv
+
+
+def get_questions(path):
+    pattern = re.compile(
+        r"Вопрос(?::|\s+\d+:)\s*(?P<question>.+?)\s*Ответ:\s*(?P<answer>.+?)(?=\n(?:Комментарий:|Источник:|Автор:|Вопрос:|$))",
+        re.DOTALL
+    )
+    questions = {}
+    count = 1
+    for dirpath, _, filenames in os.walk(path):
+        for file in filenames:
+            with open(os.path.join(dirpath, file), encoding='KOI8-R') as f:
+                matches = pattern.finditer(f.read())
+                for match in matches:
+                    questions[f'question{count}'] = {
+                        'question': match.group('question'),
+                        'answer': match.group('answer')
+                    }
+                    count += 1
+    return questions
+
+
+def handle_message_from_user(event, vk_api, keyboard, questions, db):
+    if event.text == 'Новый вопрос':
+        question_number = random.randint(1, len(questions))
+        question = questions[f'question{question_number}']
+        db.set(event.user_id, question['answer'])
+        vk_api.messages.send(
+            user_id=event.user_id,
+            message=question['question'],
+            random_id=get_random_id(),
+            keyboard=keyboard.get_keyboard()
+        )
+    elif event.text == 'Сдаться':
+        answer = db.get(event.user_id).decode('utf-8')
+        text = f'Вот правильный ответ: {answer}\n' \
+               'Нажми "Новый вопрос" для продолжения'
+        vk_api.messages.send(
+            user_id=event.user_id,
+            message=text,
+            random_id=get_random_id(),
+            keyboard=keyboard.get_keyboard()
+        )
+    else:
+        answer = db.get(event.user_id).decode('utf-8')
+        message = event.text
+        if message.lower() == re.sub(r'\s*\([^)]*\)|[.]+$', '', answer).lower():
+            text = 'Правильно! Поздравляю!' \
+                   'Для следующего вопроса нажми «Новый вопрос'
+        else:
+            text = 'Неправильно… Попробуешь ещё раз?'
+
+        vk_api.messages.send(
+            user_id=event.user_id,
+            message=text,
+            random_id=get_random_id(),
+            keyboard=keyboard.get_keyboard()
+        )
+
+
+def main():
+    questions = get_questions('quiz-questions')
+
+    db = redis.Redis(host='localhost', port=6379, db=0)
+
+    load_dotenv()
+    token = os.getenv('VK_TOKEN')
+    vk_session = vk.VkApi(token=token)
+    vk_api = vk_session.get_api()
+    longpoll = VkLongPoll(vk_session)
+
+    keyboard = VkKeyboard()
+    keyboard.add_button('Новый вопрос')
+    keyboard.add_line()
+    keyboard.add_button('Сдаться')
+
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            handle_message_from_user(event, vk_api, keyboard, questions, db)
+
+
+if __name__ == "__main__":
+    main()
